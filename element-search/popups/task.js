f1237195b6c1d5f747c0e6836d9da72ee3cc4a6d
Zaykov <zk8888m@gmail.com>
2023-03-14 13:53:40 +0300

    после доработки

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,18 +1,13 @@
 const modal = document.getElementById('modal_main');
 const modals = document.querySelectorAll('.modal');
-const modalClose = document.getElementsByClassName('modal__close');
+const modalClose = Array.from(document.getElementsByClassName('modal__close'));
 const showSuccess = document.querySelector('.show-success');
 
-let firstElement = modalClose[0];
-
-let secondElement = modalClose[2];
-
-firstElement.onclick = () => {
-	modals[0].classList.remove('modal_active');
-}
-
-secondElement.onclick = () => {
-	modals[1].classList.remove('modal_active');
+for (let i = 0; i < modalClose.length; i++) {
+    modalClose[i].onclick = function() {
+       const active = modalClose[i].closest('.modal');
+       active.classList.remove('modal_active');
+    }
 }
 
 showSuccess.onclick = () => {
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,24 +1,23 @@
 const left = document.getElementsByClassName('slider__arrow_prev')[0];
 const right = document.getElementsByClassName('slider__arrow_next')[0];
 const slides = Array.from(document.querySelectorAll('.slider__item'));
-let item = 0;
 
 left.onclick = () => {    
+    let item = slides.findIndex(element => element.className === 'slider__item slider__item_active');
     slides[item].classList.remove('slider__item_active');
     item--;
     if (item < 0) {
         item = slides.length - 1;
     }
     slides[item].classList.add('slider__item_active');
-
 }
 
 right.onclick = () => {
+    let item = slides.findIndex(element => element.className === 'slider__item slider__item_active');
     slides[item].classList.remove('slider__item_active');
     item++;
     if (item >= slides.length) {
         item = 0;
     }
     slides[item].classList.add('slider__item_active');
-  
 }
